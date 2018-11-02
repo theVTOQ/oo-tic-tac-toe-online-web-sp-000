b0b29ba21caaf7118f2cafe8d1ca65c7fd656b4b
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-02 15:35:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,4 +37,16 @@ class TicTacToe
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board(board)
+    else
+      turn
+    end
+  end
 end
