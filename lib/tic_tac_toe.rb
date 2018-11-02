36598b755b412bbad3c75f930dd4702868559292
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-02 15:34:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,5 +32,9 @@ class TicTacToe
 
   def position_taken?(index)
     !(@board[index].nil? || @board[index] == " ")
-end
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
 end
