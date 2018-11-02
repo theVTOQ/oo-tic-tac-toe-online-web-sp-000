e31722b458ac939590e43fb6180b7f1efdebfa92
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-02 15:29:35 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,4 +13,12 @@ class TicTacToe
     [6,4,2],
     [0,4,8]
   ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 end
